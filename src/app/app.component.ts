85d731c0914320bd0be86c454c5ea36dd85b9774
JohnnyMatrixxx <JohnnyMatrixxx@users.noreply.github.com>
2019-03-05 14:12:43 -0400

    Set up firebase.initializeApp
     apiKey, authDomain

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCVqAZKJoP6QQXyog_nIobc32nTF562H0E",
+      authDomain: "ang7-spa-demo-backend.firebaseapp.com"
+    });
+  }
+
   title = 'ang7-spa-demo';
 }
